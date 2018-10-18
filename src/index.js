49174deafac9a4e34ae210719ebfe03549e17b5c
Tom Workman <tomworkman@Toms-MacBook-Pro.local>
2018-10-17 22:38:01 -0500

    fix another bug :/

--- src/index.js
+++ src/index.js
@@ -54,9 +54,9 @@ function todos(state = [], action) {
 // Goals Reducer to give our app's state something else to track
 function goals(state = [], action) {
   switch(action.type) {
-    case: 'ADD_GOAL' :
+    case 'ADD_GOAL' :
       return state.concat([action.goal])
-    case: 'REMOVE_GOAL' :
+    case 'REMOVE_GOAL' :
       return state.filter((goal) => goal.id !== action.id)
     default :
       return state
