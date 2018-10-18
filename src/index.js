22a6e118711c2d5530c3ebccd23f97afa55576fb
Tom Workman <tomworkman@Toms-MacBook-Pro.local>
2018-10-17 22:37:09 -0500

    add test dispatchers and fix bug

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function createStore(reducer) {
 // APP CODE
 // Pure function/Reducer that takes in state and an action to update the state
 function todos(state = [], action) {
-  switch (action.type) {
+  switch(action.type) {
     case 'ADD_TODO' :
       return state.concat([action.todo])
     case 'REMOVE_TODO' :
@@ -51,8 +51,30 @@ function todos(state = [], action) {
   }
 }
 
+// Goals Reducer to give our app's state something else to track
+function goals(state = [], action) {
+  switch(action.type) {
+    case: 'ADD_GOAL' :
+      return state.concat([action.goal])
+    case: 'REMOVE_GOAL' :
+      return state.filter((goal) => goal.id !== action.id)
+    default :
+      return state
+  }
+}
 
-const store = createStore(todos)
+function app(state = {}, action) {
+  // return a state object that has todos and goals properties
+  // value of those properties is a reducer function that manages
+  // slices of that state
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
+}
+
+// pass createStore the main app function/root reducer
+const store = createStore(app)
 // event listener
 store.subscribe(() => {
   console.log('The new state is: ', store.getState())
@@ -71,10 +93,58 @@ store.dispatch({
   type: 'ADD_TODO',
   todo: {
     id: 1,
-    name: 'something else',
+    name: 'Learn Redux some more!',
+    complete: false,
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 1,
+    name: 'Go roll some jiu-jitsu',
     complete: true,
   }
 })
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  todo: {
+    id: 1,
+  }
+})
+
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  todo: {
+    id: 1,
+  }
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+  }
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  todo: {
+    id: 1,
+    name: 'Learn Redux some more!',
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_GOAL',
+  todo: {
+    id: 0,
+    name: 'Learn Redux some more!',
+  }
+})
+
 // const unsubscribe = store.subscribe(() => {
 //   console.log('The store changed.')
 // })
